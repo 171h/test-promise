9b99e63929fceee11ef84affb326de6e9e542cbe
H171 <1091088708@qq.com>
2023-08-12 23:29:43 +0800

    chore: update `splitpanes`

---
+++ components/TestSplitpanes.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Splitpanes, Pane } from 'splitpanes'
+import 'splitpanes/dist/splitpanes.css'
+
+</script>
+
+<template>
+  <h1>Test Splitpanes</h1>
+  <div class="h-48">
+    <splitpanes class="default-theme">
+      <pane v-for="i in 3" :key="i">
+        <div>{{ i }}</div>
+      </pane>
+    </splitpanes>
+  </div>
+</template>
