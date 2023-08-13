48344a17b5bcc154c9d9388824dabeca20e13641
H171 <1091088708@qq.com>
2023-08-13 16:44:32 +0800

    chore: update

--- components/TestSplitpanes.vue
+++ components/TestSplitpanes.vue
@@ -7,10 +7,46 @@ import 'splitpanes/dist/splitpanes.css'
 <template>
   <h1>Test Splitpanes</h1>
   <div class="h-48">
-    <splitpanes class="default-theme">
+    <splitpanes >
       <pane v-for="i in 3" :key="i">
         <div>{{ i }}</div>
       </pane>
     </splitpanes>
   </div>
 </template>
+<style>
+.splitpanes {
+  background-color: transparent;
+}
+
+.splitpanes__splitter {
+  background-color: #ccc;
+  position: relative;
+}
+
+.splitpanes__splitter:before {
+  content: '';
+  position: absolute;
+  left: 0;
+  top: 0;
+  transition: opacity 0.4s;
+  background-color: rgb(128, 0, 128, 0.3);
+  opacity: 0;
+  z-index: 1;
+}
+
+.splitpanes__splitter:hover:before {
+  opacity: 1;
+}
+
+.splitpanes--vertical>.splitpanes__splitter:before {
+  right: -5px;
+  height: 100%;
+}
+
+.splitpanes--horizontal>.splitpanes__splitter:before {
+  bottom: -5px;
+  width: 100%;
+}
+
+</style>
---
+++ test/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest'
+
+describe('test', () => {
+  it('should be ok', () => {
+    expect(1).toBe(1)
+  })
+})
