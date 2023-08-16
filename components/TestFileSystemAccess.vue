ecb6ef4e1fac61c6ee4e76eb10ad6165394a324f
H171 <1091088708@qq.com>
2023-08-16 22:01:01 +0800

    chore: update

--- components/TestFileSystemAccess.vue
+++ components/TestFileSystemAccess.vue
@@ -3,25 +3,46 @@ const props = defineProps<{
   modelValue?: boolean
 }>()
 
-const { isSupported, data, file, fileName, fileMIME, fileSize, fileLastModified, create, open, save, saveAs, updateData } = useFileSystemAccess({})
+const fsa = useFileSystemAccess({
+  dataType: 'ArrayBuffer',
+  types: [
+    {
+      accept: { 'application/g-zip': ['.mddoc'] },
+      description: 'mddoc 文件',
+    },
+  ],
+  excludeAcceptAllOption: true,
+})
+
+const data = fsa.data
+const text = computed({
+  get: () => new TextDecoder().decode(data.value as ArrayBuffer),
+  set: (v) => {
+    data.value = new TextEncoder().encode(v)
+  },
+})
+const u8 = computed(() => new Uint8Array(data.value as ArrayBuffer))
 </script>
 
 <template>
   <div>
-    <UButton label="create" @click="create" />
-    <UButton label="open" @click="open" />
-    <UButton label="save" @click="save" />
-    <UButton label="saveAs" @click="saveAs" />
-    <UButton label="updateData" @click="updateData" />
+    <UButton label="create" @click="fsa.create" />
+    <UButton label="open" @click="fsa.open" />
+    <UButton label="save" @click="fsa.save" />
+    <UButton label="saveAs" @click="fsa.saveAs" />
+    <UButton label="updateData" @click="fsa.updateData" />
+    <UButton label="console" @click="console.dir(fsa)" />
+    <UButton label="console data" @click="console.log(data)" />
+    <UButton label="console u8" @click="console.log(u8)" />
 
     <div>
-      <div>isSupported: {{ isSupported }}</div>
-      <div>data: <input v-model="data" type="text"></div>
-      <div>file: {{ file }}</div>
-      <div>fileName: {{ fileName }}</div>
-      <div>fileMIME: {{ fileMIME }}</div>
-      <div>fileSize: {{ fileSize }}</div>
-      <div>fileLastModified: {{ fileLastModified }}</div>
+      <div>isSupported: {{ fsa.isSupported }}</div>
+      <div>data: <input v-model="text" type="text"></div>
+      <div>file: {{ JSON.stringify(fsa.file) }}</div>
+      <div>fileName: {{ fsa.fileName }}</div>
+      <div>fileMIME: {{ fsa.fileMIME }}</div>
+      <div>fileSize: {{ fsa.fileSize }}</div>
+      <div>fileLastModified: {{ fsa.fileLastModified }}</div>
     </div>
   </div>
 </template>
